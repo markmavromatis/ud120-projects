b6901ce4867f54ca0a8c03fb7510f5f3fef532c2
Mark Mavromatis <markm@palamida.com>
2016-07-26 17:49:09 -0700

    Bulk tester for testing different classifiers and feature combinations

---
+++ final_project/mark_scripts/bulk_tester.py
@@ -0,0 +1,174 @@
+#!/usr/bin/pickle
+
+""" a basic script for importing student's POI identifier,
+    and checking the results that they get from it
+
+    requires that the algorithm, dataset, and features list
+    be written to my_classifier.pkl, my_dataset.pkl, and
+    my_feature_list.pkl, respectively
+
+    that process should happen at the end of poi_id.py
+"""
+
+import pickle
+import sys
+import numpy
+
+from sklearn.cross_validation import StratifiedShuffleSplit
+# Naive Bayes Classifier
+from sklearn.naive_bayes import GaussianNB
+# Decision Tree Classifier
+from sklearn import tree
+# Random Forest Estimator
+from sklearn.ensemble import RandomForestClassifier
+
+sys.path.append("../../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+classifiers = ["naive_bayes"];
+
+PERF_FORMAT_STRING = "\
+\tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
+Recall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
+RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
+\tFalse negatives: {:4d}\tTrue negatives: {:4d}"
+
+PERF_FORMAT_CSV = "\
+, {:>0.{display_precision}f}, {:>0.{display_precision}f}, {:>0.{display_precision}f}, {:>0.{display_precision}f}, {:>0.{display_precision}f}"
+RESULTS_FORMAT_CSV = ", {:4d}, {:4d}, {:4d}, {:4d}, {:4d}"
+
+
+def test_classifier(clf, dataset, feature_list, results_summary, folds=1000):
+    data = featureFormat(dataset, feature_list, sort_keys=True)
+    labels, features = targetFeatureSplit(data)
+    cv = StratifiedShuffleSplit(labels, folds, random_state=42)
+    true_negatives = 0
+    false_negatives = 0
+    true_positives = 0
+    false_positives = 0
+    for train_idx, test_idx in cv:
+        features_train = []
+        features_test = []
+        labels_train = []
+        labels_test = []
+        for ii in train_idx:
+            features_train.append(features[ii])
+            labels_train.append(labels[ii])
+        for jj in test_idx:
+            features_test.append(features[jj])
+            labels_test.append(labels[jj])
+
+        ### fit the classifier using training set, and test on test set
+        clf.fit(features_train, labels_train)
+        predictions = clf.predict(features_test)
+        for prediction, truth in zip(predictions, labels_test):
+            if prediction == 0 and truth == 0:
+                true_negatives += 1
+            elif prediction == 0 and truth == 1:
+                false_negatives += 1
+            elif prediction == 1 and truth == 0:
+                false_positives += 1
+            elif prediction == 1 and truth == 1:
+                true_positives += 1
+            else:
+                print "Warning: Found a predicted label not == 0 or 1."
+                print "All predictions should take value 0 or 1."
+                print "Evaluating performance for processed predictions:"
+                break
+    try:
+        total_predictions = true_negatives + false_negatives + false_positives + true_positives
+        accuracy = 1.0 * (true_positives + true_negatives) / total_predictions
+        precision = 1.0 * true_positives / (true_positives + false_positives)
+        recall = 1.0 * true_positives / (true_positives + false_negatives)
+        f1 = 2.0 * true_positives / (2 * true_positives + false_positives + false_negatives)
+        f2 = (1 + 2.0 * 2.0) * precision * recall / (4 * precision + recall)
+        classifierType = ""
+        if isinstance(clf, GaussianNB):
+            classifierType = "Naive Bayes"
+        elif isinstance(clf, tree.DecisionTreeClassifier):
+            classifierType = "Decision Tree"
+        elif isinstance(clf, RandomForestClassifier):
+            classifierType = "Random Forest"
+        else:
+            raise Exception("Unsupported classifier type: " + type(clf))
+        results_summary.append(classifierType + ", " + ', '.join(feature_list[1:]) + ", "
+                               + PERF_FORMAT_CSV.format(accuracy, precision, recall, f1, f2, display_precision=5))
+
+        print clf
+        print PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision=5)
+        print RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives,
+                                           true_negatives)
+        print ""
+    except:
+        print "Got a divide by zero when trying out:", clf
+        print "Precision or recall may be undefined due to a lack of true positive predicitons."
+
+
+CLF_PICKLE_FILENAME = "../my_classifier.pkl"
+DATASET_PICKLE_FILENAME = "../my_dataset.pkl"
+FEATURE_LIST_FILENAME = "../my_feature_list.pkl"
+
+def load_data():
+    with open(DATASET_PICKLE_FILENAME, "r") as dataset_infile:
+        dataset = pickle.load(dataset_infile)
+    return dataset
+
+
+def main():
+    ### load up student's classifier, dataset, and feature_list
+    dataset = load_data()
+
+    classifiers = ["NaiveBayes", "DecisionTree", "RandomForest"]
+
+    # print "Feature list is: ", feature_list
+    ### Run testing script
+
+    important_features = ["total_payments", "total_stock_value", "salary", "bonus", "exercised_stock_options", "shared_receipt_with_poi","to_messages","from_messages", "to_poi_ratio"]
+    # important_features = ["salary", "bonus"]
+    print "# Rows = " , len(dataset)
+#    print(dataset)
+
+    # For missing fields, fill in 0
+    for row_key in dataset:
+        a_row = dataset[row_key]
+        for each_feature in important_features:
+            if each_feature not in a_row:
+                a_row[each_feature] = 1
+
+    # Remove outliers due to bad data including
+    #Totals Row
+    del dataset['TOTAL']
+    # Some travel agency
+    del dataset['THE TRAVEL AGENCY IN THE PARK']
+
+    # How many rows left?
+    print "Rows after cleanup: " , len(dataset)
+
+    results_summary = []
+    results_summary.append("Classifier,Field1,Field2,,accuracy, precision, recall, f1, f2")
+    # Pick 2 of each item in the important features list
+    for each_classifier in classifiers:
+        clf = 0
+        if each_classifier == "NaiveBayes":
+            clf = GaussianNB()
+        elif each_classifier == "DecisionTree":
+            clf = tree.DecisionTreeClassifier()
+        elif each_classifier == "RandomForest":
+            clf = RandomForestClassifier(n_estimators=10)
+        else:
+            raise Exception("Unsupported classifier type: " + each_classifier)
+
+        for i in range(len(important_features) - 2):
+            for j in range(i + 1, len(important_features) - 1):
+                for k in range(j + 1, len(important_features)):
+                    iterate_features = [important_features[i], important_features[j], important_features[k]]
+                # print "Testing NB classifier for features: " + str(iterate_features)
+                classifier_features = ["poi"] + iterate_features
+                print "CLF Features: " + str(classifier_features)
+                test_classifier(clf, dataset, classifier_features, results_summary)
+
+    print "Here are the results:"
+    print "\n".join(results_summary)
+
+if __name__ == '__main__':
+    main()
